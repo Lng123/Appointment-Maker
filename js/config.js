f0ed6e470577af999e7bc221aba3c478f5d53dc5
John <johnlee46@gmail.com>
2020-03-18 16:38:47 -0700

    firstcommit
    firstcommit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_19tgQLHZU', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7m8vrb63ukbs0d3p5dbdhakgim', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://xa9svb85nl.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
